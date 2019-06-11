995d5bd60345fb9874f55ac25a7831a8855a67ab
juliagorova <juliagorova@MacBook-Air-Julia.local>
2019-06-11 17:07:37 +0300

    Added responsive design, reduced image sizes, bugs fixed.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+// Init Google Map
+function initMap() {
+  var bakeryLocation = { lat: 49.8410154, lng: 24.0316713 };
+  var map = new google.maps.Map(
+      document.getElementById('map'), { zoom: 16, center: bakeryLocation });
+  var marker = new google.maps.Marker({ position: bakeryLocation, map: map });
+}
+
+//Scroll to top functionality
 $(document).ready(function () {
   var scrollTop = $("#scrollToTop");
 
